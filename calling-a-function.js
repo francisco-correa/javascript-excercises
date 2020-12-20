7f60aa710e49c0aeedec10d973f4e738d37f089c
Francisco Correa <75753132+francisco-correa@users.noreply.github.com>
2020-12-19 22:21:09 -0300

    Update calling-a-function.js
    Agregando una "llamada más"

--- calling-a-function.js
+++ calling-a-function.js
@@ -6,7 +6,9 @@ function calculateArea(length,edge)
 let squareArea1 = calculateArea(4,4);
 let squareArea2 = calculateArea(2,2);
 let squareArea3 = calculateArea(5,5);
+let squareArea4 = calculateArea(3,3);
 
 console.log(squareArea1);
 console.log(squareArea2);
 console.log(squareArea3);
+console.log(squareArea4);
