1e4c88c6e8ccb9471f21908dfe7083700f723237
Francisco Correa <75753132+francisco-correa@users.noreply.github.com>
2020-12-19 22:18:24 -0300

    Create calling-a-function.js
    cálculo de área de una figura

---
+++ calling-a-function.js
@@ -0,0 +1,12 @@
+function calculateArea(length,edge)
+{
+	return length * edge;
+}
+
+let squareArea1 = calculateArea(4,4);
+let squareArea2 = calculateArea(2,2);
+let squareArea3 = calculateArea(5,5);
+
+console.log(squareArea1);
+console.log(squareArea2);
+console.log(squareArea3);
